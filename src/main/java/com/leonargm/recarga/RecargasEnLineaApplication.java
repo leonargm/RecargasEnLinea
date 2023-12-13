619de1e0a832d56f6c11323ea469ce5847e4a12d
leonargm <leonargm@gmail.com>
2023-12-13 12:19:33 -0500

    initial update

---
+++ src/main/java/com/leonargm/recarga/RecargasEnLineaApplication.java
@@ -0,0 +1,13 @@
+package com.leonargm.recarga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecargasEnLineaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecargasEnLineaApplication.class, args);
+	}
+
+}
